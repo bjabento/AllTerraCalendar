962a44238790c46f2af2857ad15adc656996e979
Gonçalo Lopes <goncalo-lopes@hotmail.com>
2023-06-29 08:24:16 +0100

    project fixes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.get('/', function(req, res) {
     res.render('login');
 });
 
+
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log('Listening on port 3000!'));
 
@@ -18,54 +19,65 @@ app.use(express.static(__dirname + '/css'));
 const Sequelize = require('sequelize');
 const moment = require('moment');
 const db = require('./configs/Database');
-/*const User = require('./models/User');
-const Report = require('./models/Report');
+const User = require('./models/Users');
+/*const Report = require('./models/Report');
 const Locals = require('./models/Locals')
 const Feedback = require('./models/Feedback');
 const Admin = require('./models/Admins');*/
 const { Session } = require('inspector');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
     secret: "secret",
-    resave: false,
-    saveUninitialized: false,
-    adminType: 2,
-    cookie:{
+    resave: true,
+    saveUninitialized: true,
+    userID: 0,
+    cookie: {
         expires: new Date(Date.now() + 3600000)
     }
-}))
+}));
 
-const redirectLogin = (req, res, next) => {
-    if(req.session.userID == undefined){
-        res.redirect('/login')
-    }else{
-        next()
+function redirectLogin(req, res, next) {
+    console.log('Cheguei');
+    //console.log(req.session.userID);
+    if (!req.session) {
+        res.redirect('/');
+    } else {
+        next();
     }
 }
 
+app.get('/holidayhistory', redirectLogin, function(req, res) {
+    console.log('Cheguei render');
+    res.render('holidayhistory');
+});
+
 app.post('/loginRequest', (req, res) => {
     var lMail = req.body.email;
-    var lPass = req.body.pass;
+    var lPass = req.body.password;
 
-    Admin.findAll({
+    User.findAll({
         where: {
             email: lMail
         }
     }).then(user => {
         console.log(user);
-        console.log(typeof(user[0].dataValues.pass))
-        console.log(typeof(lPass))
-        if (user.length == 1 && user[0].dataValues.pass.trim() == lPass.trim()){
-            if(user[0].dataValues.tipo == 0){
+        console.log(typeof (user[0].dataValues.password))
+        console.log(typeof (lPass))
+        if (user.length == 1 && user[0].dataValues.password.trim() == lPass.trim()) {
+
+            req.session.userID = user[0].dataValues.id;
+            console.log(req.session.userID);
+            /*if(user[0].dataValues.tipo == 0){
                 req.session.adminType = 0
             }else{
                 req.session.adminType = 1
-            }
-            res.redirect('/index');
-        }else{
+            }*/
+            res.render('holidayhistory');
+            //res.redirect('/holidayhistory');
+        } else {
             res.redirect('/');
         }
     }).catch(err => res.redirect('/'))
-})
+});
